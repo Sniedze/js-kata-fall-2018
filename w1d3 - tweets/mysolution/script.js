10181bf7d05d08e677a65f25c597044548299aa2
Ulrika <u.sniedze@gmail.com>
2018-08-30 18:32:30 +0200

    Adds fetching code in JS

--- w1d3 - tweets/mysolution/script.js
+++ w1d3 - tweets/mysolution/script.js
@@ -2,36 +2,18 @@
 const main = document.querySelector("main");
 const template = document.querySelector("#template");
 
-fetch("jason.json")
-  .then(result => result.json())
-  .then(data => createContainers(data));
+fetch("https://kea-alt-del.dk/twitter/api/")
+  .then(e => {
+    return e.json();
+  })
+  .then(showTweets);
 
-function createContainers(tweets) {
-  tweets.forEach(tweet => {
-    let section = document.createElement("section");
-
-    section.id = tweet.coreArea;
-  });
-  main.appendChild(section);
-}
-fetch("jason.json")
-  .then(result => result.json())
-  .then(data => show(data));
-
-function show(data) {
-  data.forEach(element => {
-    const section = document.querySelector("#" + element.coreArea);
+function showTweets(data) {
+  data.forEach(data => {
     let clone = template.cloneNode(true).content;
 
-    clone.querySelector(".profile-image").textContent = element.coreArea;
-
-    clone.querySelector(".author").textContent = element.content;
-    clone.querySelector(".date").textContent = "Knowledge";
-    clone.querySelector(".tweet-text").textContent = "Skills";
-    clone.querySelector(".link").textContent = "Competences";
-    clone.querySelector(".tweet-image").textContent = element.content;
-    clone.querySelector(".author").textContent = element.content;
-
+    clone.querySelector(".date").textContent = data.statuses.created_at;
+    clone.querySelector(".tweet-text").textContent = data.statuses.text;
     section.appendChild(clone);
   });
 }
