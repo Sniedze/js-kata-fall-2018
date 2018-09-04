9dd6ea38ac97803b0deb5afafc7d2d8760c5667c
Ulrika <u.sniedze@gmail.com>
2018-09-04 19:11:36 +0200

    Adds h2

--- w2d1 - listMethods/mysolution/script.js
+++ w2d1 - listMethods/mysolution/script.js
@@ -11,16 +11,20 @@ function init() {
   modifyArray = document.querySelectorAll("#modify ul li");
   infoArray = document.querySelectorAll("#info ul li");
   newArray = document.querySelectorAll("#newarray ul li");
+
   listMethods(modifyArray);
   listMethods(infoArray);
   listMethods(newArray);
 }
 
 function displayElement(element) {
-  console.log(element.textContent);
+  console.log("\u2022" + element.textContent);
 }
 
 function listMethods(array) {
+  let headingTwo = array[0].parentElement.parentElement.querySelector("h2")
+    .textContent;
+  console.log(headingTwo);
   for (i = 0; i < array.length; i++) {
     displayElement(array[i]);
   }
